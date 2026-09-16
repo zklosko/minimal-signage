9841a3f7d49a566696ce75ec85622b02ee86ff85
zklosko <31484611+zklosko@users.noreply.github.com>
2026-09-15 22:42:03 -0400

    fixing db url issue

--- db/index.ts
+++ db/index.ts
@@ -2,5 +2,5 @@ import { createClient } from '@libsql/client'
 import path from 'node:path'
 import { drizzle } from 'drizzle-orm/libsql'
 
-const client = createClient({ url: path.resolve(process.cwd(), 'signage.db') })
+const client = createClient({ url: "file:" + path.resolve(process.cwd(), 'signage.db') })
 export const db = drizzle({ client })
